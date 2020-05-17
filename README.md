745258cb8dec5fb830c0e3c49dfc8ec410b53ec8
Dabin <dorothy9795@gmail.com>
2020-05-17 21:34:47 +0900

    이제 리터럴 문자열을 정확히 찾아낸다!

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # SourceCodeKoreanScanner
 파일 내 한글이 포함된 리터럴 문자열을 검색해서 추출, 특정 문자열로 대체 할 수 있는 툴
+
+```
+node src/app.js pullout "C:\git\Moai\GDScripts"
+```
--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
+const fsPromises = fs.promises;
 const path = require('path');
 const argv = require('yargs')
-    .command('pullout [dir] [options]', '파일 내 한글을 추출합니다', (yargs) => {
+    .command('pullout [dir]', '파일 내 한글을 추출합니다', (yargs) => {
         yargs
             .positional('dir', {
                 describe: '추출할 디렉토리 or 파일 경로'
@@ -14,39 +15,78 @@ const argv = require('yargs')
         }
     })
     .argv;
-    
-const fileSheet = [];
-const regExp = new RegExp(argv.regex);
-regExp.global = argv.global || false;
-try {
-    const files = fs.readdirSync(argv.dir);
-    files.forEach((filename)=>{
-        const textSheet = [];
-        const filepath = path.join(argv.dir, filename);
-        var lines = require('fs').readFileSync(filepath, 'utf-8').split('\n').filter(Boolean);
-        lines.forEach((line, lineNum)=>{
-            const pullOutText = line.match(regExp);
-            if (pullOutText) {
-                pullOutText.forEach((sentence)=>{
-                    textSheet.push({
-                        "line": lineNum,
-                        "text": sentence
-                    });
-                });
-            }
-        });
-        if (textSheet.length > 0) {
-            fileSheet.push({
-                "file": filename,
-                "textSheet": textSheet
-            });
-        } else {
-            console.log(`${filename} 파일에서는 추출할 텍스트가 없습니다`)
+
+const readFileContents = async (dir) => {
+    const bufferToString = (buffer) => buffer.toString().split('\n');
+    const filePath = (dir, fileName) => path.join(dir, fileName);
+    const files = await fsPromises.readdir(dir);
+    const buffers = await Promise.all(files.map((fileName) => fsPromises.readFile(filePath(dir, fileName))));
+    const fileDatas = files.map((fileName, index) => ({'fileName': fileName, 'contents': bufferToString(buffers[index])}));
+    return fileDatas;
+};
+
+const findLiteralTextList = (text, regex) => {
+    const covers = ["\'", "\""];
+    const findTextList = [];
+    let pivot = covers[0];
+    while (pivot = covers.find((cover)=>text.includes(cover))) {
+        const startIndex = text.indexOf(pivot);
+        text = text.substr(startIndex+1);
+        const endIndex = text.indexOf(pivot);
+        const findText = text.substr(0, endIndex);
+        text = text.substr(endIndex+1);
+        if (!regex || regex.test(findText)) {
+            findTextList.push(findText);
         }
-    });
-} catch (err) {
-    console.log(err.message)
+    }
+    return findTextList;
+};
+
+if (argv.dir) {
+    (async () => {
+        const fileContents = await readFileContents(argv.dir);
+        fileContents.forEach(({fileName, contents})=>{
+            contents.forEach((line)=>{
+                const koreanContents = findLiteralTextList(line, /[가-힣]/g);
+                if (koreanContents && koreanContents.length > 0) {
+                    console.log(koreanContents);
+                };
+            })
+        });
+    })();
 }
 
-fs.writeFileSync("pullout.json", JSON.stringify(fileSheet));
-console.log("추출 완료");
+// const fileSheet = [];
+// try {
+//     const files = fs.readdirSync(argv.dir);
+//     files.forEach((filename)=>{
+//         const textSheet = [];
+//         const filepath = path.join(argv.dir, filename);
+//         var lines = require('fs').readFileSync(filepath, 'utf-8').split('\n').filter(Boolean);
+//         lines.forEach((line, lineNum)=>{
+//             const startsWith = ["\'", "\""]
+//             const pullOutText = line.startsWith("\'") || line.startsWith("\'")
+//             if (pullOutText) {
+//                 pullOutText.forEach((sentence)=>{
+//                     textSheet.push({
+//                         "line": lineNum,
+//                         "text": sentence
+//                     });
+//                 });
+//             }
+//         });
+//         if (textSheet.length > 0) {
+//             fileSheet.push({
+//                 "file": filename,
+//                 "textSheet": textSheet
+//             });
+//         } else {
+//             console.log(`${filename} 파일에서는 추출할 텍스트가 없습니다`)
+//         }
+//     });
+// } catch (err) {
+//     console.log(err.message)
+// }
+
+// fs.writeFileSync("pullout.json", JSON.stringify(fileSheet));
+// console.log("추출 완료");
